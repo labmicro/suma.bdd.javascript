ad292c026777305f83e89dc88d164a4433fb222f
Esteban Volentini <evolentini@equiser.com.ar>
2020-11-02 15:34:04 -0300

    Implementación de los pasos para la primera prueba

---
+++ features/steps_definitions/suma_steps.js
@@ -0,0 +1,17 @@
+const { Before, Given, When, Then } = require('@cucumber/cucumber')
+const expect = require("chai").expect;
+const suma = require("../../module/suma.js");
+
+let contexto = {};
+
+When('efectuo una suma con saturacion de los valores {int} y {int}', function (acumulador, operando) {
+    contexto.resultado = suma.acumular(acumulador, operando);
+});
+
+Then('no hay ningun truncamiento', function () {
+    expect(contexto.resultado.error).to.be.equal(0);
+});
+
+Then('el resultado de la suma es {int}', function (acumulado) {
+    expect(contexto.resultado.acumulador).to.be.equal(acumulado);
+});

7e37dd80eeaf38da9e57a4a50d734dc4dcd60196
Esteban Volentini <evolentini@equiser.com.ar>
2020-11-02 21:12:41 -0300

    Primera implementación de la función promedio para el caso que no desborda

---
+++ module/promedio.js
@@ -0,0 +1,14 @@
+const suma = require("./suma.js");
+
+exports.promediar = (valores) => {
+
+    var acumulador = 0;    
+    for(var indice = 0; indice < valores.length; indice++) {
+        var resultado = suma.acumular(acumulador, valores[indice]);
+        acumulador = resultado.acumulador;
+    }
+
+    let promedio = acumulador / valores.length;
+    return {promedio: promedio, error: false};
+}
+

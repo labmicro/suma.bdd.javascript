b8f710c88b3bfe5f660fc1d33fe570a497f89709
Esteban Volentini <evolentini@equiser.com.ar>
2020-11-02 21:14:01 -0300

    Implementación de la nueva funcionalidad en la función promedio.

--- module/promedio.js
+++ module/promedio.js
@@ -5,6 +5,9 @@ exports.promediar = (valores) => {
     var acumulador = 0;    
     for(var indice = 0; indice < valores.length; indice++) {
         var resultado = suma.acumular(acumulador, valores[indice]);
+        if (resultado.error != 0) {
+            return {promedio: 0, error: true};
+        }
         acumulador = resultado.acumulador;
     }
 
